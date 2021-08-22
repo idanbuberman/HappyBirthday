3de2abbd61ea10c101710834ed733d3733167e4e
Idan Boberman <idanbo@perfectomobile.com>
2021-08-22 13:52:49 +0300

    most views on screen, need to fix ronaldo's picture sizing

--- HappyBirthday/HappyBirthday/BirthdayViewController.swift
+++ HappyBirthday/HappyBirthday/BirthdayViewController.swift
@@ -7,7 +7,42 @@
 //
 
 import UIKit
-
+//import Darwin
 class BirthdayViewController: UIViewController {
     
+    @IBOutlet weak var shareTheNewsButton: UIButton!
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var uploadPictureImageView: UIImageView!
+    
+    @IBOutlet weak var age: UIImageView!
+//    @IBOutlet weak var uploadOriginX: NSLayoutConstraint!
+//    @IBOutlet weak var uploadOriginY: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        shareTheNewsButton.layer.cornerRadius = shareTheNewsButton.frame.size.height / 2
+//
+//        let radius: CGFloat = uploadPictureImageView.frame.size.width / 2
+//        let pos = calcButtonPosition(radius: Float(radius), angle: 45)
+//        uploadOriginX.constant = pos.x
+//        uploadOriginY.constant = pos.y
+//    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        let radius: CGFloat = uploadPictureImageView.frame.size.width / 2
+//        let pos = calcButtonPosition(radius: Float(radius), angle: 45)
+//
+//    }
+
+    func calcButtonPosition(radius: Float, angle: Float) -> CGPoint {
+        let x: CGFloat = CGFloat(radius * cos(angle))
+        let y: CGFloat = CGFloat(radius * sin(angle))
+        return CGPoint(x: x, y: y)
+    }
 }
